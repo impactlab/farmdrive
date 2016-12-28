bf8644504f3aa7607030727cb1e41410722e0d51
Peter Bull <pjbull@gmail.com>
2016-12-28 09:32:42 -0600

    Downloading, merging, and cropping planet images

--- src/data/download_planet.py
+++ src/data/download_planet.py
@@ -1,4 +1,5 @@
 import collections
+from glob import glob
 from itertools import compress
 import json
 import os
@@ -8,6 +9,10 @@ import traceback
 
 import click
 import dotenv
+import rasterio
+from rasterio import features
+from rasterio.merge import merge
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from tqdm import tqdm
@@ -63,7 +68,6 @@ def query_for_aois(county_name, crop_table, crop_name):
     # Execute the query in the session
     result = session.execute(query)
 
-    # for now, just get the first result. ultimately, we'll need them all.
     aoi_raster = result.fetchall()
     return aoi_raster
 
@@ -199,12 +203,89 @@ def download_tiles_from_aoi(planet_query,
             fail_log.write(failed_ids)
         print("Wrote scenes that failed to download to {}".format(fail_path))
 
+    return scene_ids
+
+
+def merge_scenes(scene_ids, asset_dir, county_pixel_dir, asset_type):
+    paths = [os.path.join(asset_dir, '{}_{}.tif'.format(sid, asset_type)) \
+             for sid in scene_ids]
+
+    vrt_path = os.path.join(county_pixel_dir, 'mosaic.vrt')
+    gj_path = os.path.join(county_pixel_dir, 'geojson_epsg32637.geojson')
+
+    pixel_id = os.path.split(county_pixel_dir)[1]
+    print("PID: ", pixel_id)
+    output_tiff = os.path.join(county_pixel_dir,
+                               pixel_id + '.tif')
+
+    try:
+        check_output(['gdalbuildvrt', '-overwrite', vrt_path] + paths,
+                      stderr=STDOUT)
+    except CalledProcessError as e:
+        print(e.output)
+        raise
+
+    try:
+        check_output(['gdalwarp',
+                      '-of',
+                      'GTiff',
+                      '-cutline',
+                      gj_path,
+                      '-crop_to_cutline',
+                      '-overwrite',
+                      vrt_path,
+                      output_tiff],
+                     stderr=STDOUT)
+
+    # print("Wrote merged tiff to {}".format(output_tiff))
+
+    except CalledProcessError as e:
+        print(e.output)
+        raise
+
+
+
+
+def merge_scenes_rasterio(scene_ids, asset_dir, county_pixel_dir, asset_type):
+    paths = [glob(os.path.join(asset_dir, '{}_{}.tif')
+                         .format(sid, asset_type))[0] for sid in scene_ids]
+
+    planet_crs = {'proj': 'utm',
+                  'zone': 37,
+                  'ellps': 'WGS84',
+                  'datum': 'WGS84',
+                  'units': 'm',
+                  'no_defs': True}
+
+    srcs = [rasterio.open(p, crs=planet_crs) for p in paths]
+
+    geojson_proj_file = os.path.join(county_pixel_dir,
+                                     'geojson_epsg32637.geojson')
+    with open(geojson_proj_file) as gj_file:
+        reprojgeoj = json.load(gj_file)
+
+    out_image, out_transform = merge(srcs, bounds=features.bounds(reprojgeoj))
+
+    # save the resulting raster
+    out_meta = srcs[0].meta.copy()
+    out_meta.update({"driver": "GTiff",
+                     "height": out_image.shape[1],
+                     "width": out_image.shape[2],
+                     "transform": out_transform})
+
+    pixel_id = os.path.dirname(county_pixel_dir)
+    raster_merged_path = os.path.join(county_pixel_dir,
+                                      pixel_id + '.tif')
+
+    with rasterio.open(raster_merged_path, "w", **out_meta) as dest:
+        dest.write(out_image)
+
 
 @click.command()
 @click.argument('county_name')
 @click.argument('crop_table')
 @click.argument('crop_name')
-@click.option('--aoi_selector', default=None, type=int, help='Index of aoi to use if not all.')
+@click.option('--aoi_selector', default=None, type=str, help='Index of aoi to use if not all.')
 @click.option('--min_date', default='', help='Start date in ISO8601')
 @click.option('--max_date', default='', help='End date in ISO8601')
 @click.option('--cloud_cover', default='', help='Percent cloud cover allowed')
@@ -226,14 +307,21 @@ def download_county_crop_tiles(county_name,
     geojson_aois = query_for_aois(county_name, crop_table, crop_name)
 
     if aoi_selector:
-        geojson_aois = geojson_aois[aoi_selector]
+        if ':' in aoi_selector:
+            mini, maxi = map(int, aoi_selector.split(':'))
+            geojson_aois = geojson_aois[mini:maxi]
+        else:
+            geojson_aois = geojson_aois[int(aoi_selector)]
 
     if not isinstance(geojson_aois, collections.Iterable):
-        geojson_aois = [geojson_aois]
+        geojson_aois = [(geojson_aois, )]
 
     # download images for every area of interest
     failed_aois = []
     for aoi in geojson_aois:
+        if isinstance(aoi, sqlalchemy.engine.result.RowProxy):
+            aoi = aoi[0]
+
         try:
             # create directories if we need to
             county_data = os.path.join(PLANET_DATA_ROOT,
@@ -264,10 +352,13 @@ def download_county_crop_tiles(county_name,
             planet_query = build_planet_query(aoi, **extra_query_kwargs)
 
             # activate and download the tiles
-            download_tiles_from_aoi(planet_query,
-                                    asset_dir,
-                                    asset_type=asset_type,
-                                    search_type='PSOrthoTile')
+            scence_ids = download_tiles_from_aoi(planet_query,
+                                                 asset_dir,
+                                                 asset_type=asset_type,
+                                                 search_type='PSOrthoTile')
+
+            merge_scenes(scence_ids, asset_dir, county_pixel_dir, asset_type)
+
         except Exception as e:
             print('>>>>>>>>>>>>>> FAILURE TO DOWNLOAD AOI >>>>>>>>>>>>>')
             print(e)
@@ -284,5 +375,8 @@ def download_county_crop_tiles(county_name,
         print("Wrote scenes that failed to activate to {}".format(fail_path))
 
 
+
 if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
     download_county_crop_tiles()
