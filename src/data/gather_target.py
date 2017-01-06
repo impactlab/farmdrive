6abeb50d2cc9ae54eb633015f8625ad6f34aa1b6
Peter Bull <pjbull@gmail.com>
2017-01-06 14:44:36 -0500

    Add script for gathering the target data

---
+++ src/data/gather_target.py
@@ -0,0 +1,42 @@
+from glob import glob
+import json
+import os
+
+import click
+import pandas as pd
+
+
+@click.command()
+@click.argument('dir_path')
+@click.argument('output_file')
+@click.argument('input_filename')
+def gather_target_data(dir_path, output_file, input_filename, crop='maize'):
+    """ We right out the per-pixel crop values to a geojson file
+        as part of our querying and include that file in each
+        directory with an image. This method parses each geojson
+        file to get that yield value and then outputs a csv with the
+        folder name and value to `output_file`.
+    """
+    yields = []
+    ids = []
+
+    yield_str = '{}_yield'.format(crop)
+
+    for gj_path in glob(os.path.join(dir_path, '**', input_filename)):
+        with open(gj_path, 'r') as gj_file:
+            gj_data = json.load(gj_file)
+
+            crop_yield = gj_data['properties'][yield_str]
+            pixel_id = gj_data['id']
+
+            yields.append(crop_yield)
+            ids.append(pixel_id)
+
+    # write to csv
+    (pd.DataFrame({'id': ids, yield_str: yields})
+       .set_index('id')
+       .to_csv(output_file))
+
+
+if __name__ == '__main__':
+    gather_target_data()
