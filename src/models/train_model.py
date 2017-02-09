60856bb08ee343ae548b9613d136e5558e874cfe
Peter Bull <peter@drivendata.org>
2017-02-09 22:17:55 +0000

    Add season, crop to train workflow; optimize memory usage; workaround keras bug

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,6 +14,7 @@ import numpy as np
 import rasterio
 
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import GlobalAveragePooling2D, Dense
@@ -26,53 +27,74 @@ from keras.applications.inception_v3 import preprocess_input as iv3_preproc
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input as vgg_preproc
 
+
+PROJ_ROOT = os.path.abspath(os.path.join(__file__,
+                                         os.pardir,
+                                         os.pardir,
+                                         os.pardir))
+
+
+
 @click.command()
 @click.argument('image_directory')
 @click.argument('labels_path')
-@click.argument('output_path')
+@click.option('--season', default='')
 @click.option('--n_validation_samples', default=0.2)
 @click.option('--n_epoch', default=30)
 @click.option('--asset_type', default='visual')
 @click.option('--model', default='InceptionV3')
 @click.option('--loss', default='mean_absolute_error')
+@click.option('--crop', default='maize')
 def train_model(image_directory,
                 labels_path,
-                output_path,
+                season,
                 n_validation_samples,
                 n_epoch,
                 asset_type,
                 model,
-                loss):
+                loss,
+                crop):
     """
     """
     labels = pd.read_csv(labels_path, index_col=0)
 
     if model == 'InceptionV3':
+        model_name = model
         img_width = 299
         img_height = 299
         model_class = InceptionV3
         preprocess_func = iv3_preproc
         freeze_depth = 172
     elif model == 'VGG16':
+        model_name = model
         img_width = 224
         img_height = 224
         model_class = VGG16
-        preprocess_func = vgg_preproc
+        preprocess_func = lambda x: x
         freeze_depth = 25
     else:
         raise Exception('{} is an unsupported model.'.format(model))
+
+    # shuffle labels so we can do train/test split with slices
+    labels = shuffle(labels, random_state=234)
 
     images, targets = load_data(labels,
                                 img_width,
                                 img_height,
                                 asset_type,
                                 image_directory,
-                                'maize_yield')
+                                season,
+                                '{}_yield'.format(crop))
 
-    X_train, X_valid, y_train, y_valid = train_test_split(images,
-                                                          targets,
-                                                          test_size=n_validation_samples,
-                                                          random_state=2234)
+    if n_validation_samples < 1:
+        n_validation_samples = np.floor(images.shape[0] * n_validation_samples)
+
+    X_train = images[n_validation_samples:, :, :, :]
+    X_valid = images[:n_validation_samples, :, :, :]
+    y_train = targets[n_validation_samples:, :]
+    y_valid = targets[:n_validation_samples, :]
+
+    print("SHAPES", X_train.shape, X_valid.shape, y_train.shape, y_valid.shape)
 
     n_train_samples = y_train.shape[0]
     n_validation_samples = y_valid.shape[0]
@@ -103,19 +125,34 @@ def train_model(image_directory,
                    n_epoch=n_epoch,
                    loss=loss)
 
+    output_base = '{}_{}_{}_{}'.format(model_name, asset_type, season, crop)
+
+    # save model training history
+    model_training_history = pd.DataFrame(model.history.history)
+    training_history_path = os.path.join(PROJ_ROOT, 'models', output_base + '_history.csv')
+    model_training_history.to_csv(training_history_path)
+
+    # save model itself
+    output_path = os.path.join(PROJ_ROOT, 'models', output_base + '_keras.h5')
     persist_model(model, output_path)
 
 
-def load_data(labels, height, width, asset_type, data_root, target_var):
-    X = []
-    y = []
+def load_data(labels,
+              height,
+              width,
+              asset_type,
+              data_root,
+              season,
+              target_var,
+              rescale_target=False,
+              standardize_data=False):
 
-    # rescale target to 0-1
-#    labels[target_var] = ((labels[target_var] - labels[target_var].min()) /
- #       (labels[target_var].max() - labels[target_var].min()))
+    image_exists = []
+    for i, index_row in enumerate(labels.iterrows()):
+        index, row = index_row
+        if season:
+            index += '_{}'.format(season)
 
-    print('Read train images')
-    for index, row in labels.iterrows():
         f_name_format = '{}_{}_{}x{}.tif'
         filename = f_name_format.format(index,
                                         asset_type,
@@ -126,30 +163,63 @@ def load_data(labels, height, width, asset_type, data_root, target_var):
 
         if not os.path.exists(image_path):
             print('{} Does not exist. Skipping...'.format(image_path))
-            continue
+            image_exists.append(False)
+        else:
+            image_exists.append(True)
+
+    labels = labels.loc[image_exists, :]
+
+    # tensorflow ordering
+    X = np.zeros((labels.shape[0], width, height, 3), dtype=np.float32)
+    y = []
+
+    # rescale target to 0-1
+    if rescale_target:
+        labels[target_var] = ((labels[target_var] - labels[target_var].min()) /
+        (labels[target_var].max() - labels[target_var].min()))
+
+    print('Read train images')
+    for i, index_row in enumerate(labels.iterrows()):
+        index, row = index_row
+        if season:
+            index += '_{}'.format(season)
+
+        f_name_format = '{}_{}_{}x{}.tif'
+        filename = f_name_format.format(index,
+                                        asset_type,
+                                        width,
+                                        height)
+
+        image_path = os.path.join(data_root, index, filename)
 
         with rasterio.open(image_path) as src:
             if asset_type == 'visual':
             # planet images from visual asset are processed to
             # bands red, green, blue, alpha
                 r, g, b, a = src.read()
-                x = np.array([r, g, b], dtype=np.float32)
+                channels = [r, g, b]
             elif asset_type == 'analytic':
                 r, g, b, nir = src.read()
-                x = np.array([r, g, nir], dtype=np.float32)
+                channels = [r, g, nir]
 
-        X.append(x)
+        for c, vals in enumerate(channels):
+            X[i, :, :, c] = vals
+
+        del(channels)
+
         y.append([row[target_var]])
 
-    X, y = np.array(X), np.array(y)
+    y = np.array(y)
+
+    del(labels)
 
     # Normalize X in each channel
-#    for i in range(3):
-#        X[:,i,:,:] -= X[:,i,:,:].mean()
-        #X[:,i,:,:] /= X[:,i,:,:].std()
+    if standardize_data:
+        for c in range(3):
+            X[:,:,:,c] -= X[:,:,:,c].mean()
+            X[:,:,:,c] /= X[:,:,:,c].std()
 
-    # we need to set tensorflow (tf) ordering for best performance
-    return X.transpose((0, 2, 3, 1)), y
+    return X, y
 
 
 def get_pretrained_model(model_class, weights='imagenet'):
@@ -287,6 +357,10 @@ def persist_model(model, output_path):
     # need to unfreeze if we want the model to save/load properly.
     for layer in model.layers:
         layer.trainable = True
+
+    # ditch the optimizer weights, since there is a keras
+    # bug that prevents a model with these weights being loaded (#4044)
+    model.optimizer.weights = None
 
     model.save(output_path)
 
